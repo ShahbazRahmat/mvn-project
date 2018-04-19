b0da4eda1c729df37f377d48f1f5c49b32212802
Shahbaz Rahmat <shahbaz.rahmatkareem@seventechnology.co.uk>
2018-04-19 21:54:23 +0500

    @return in comments

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,8 @@ public class Greeter {
   }
 
   /**
-   * This is a greet function.
+   * @param it is a greet function.
+   * @return greeting string
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
